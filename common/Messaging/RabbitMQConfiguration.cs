8660b900fd10da11f785bb80fe507ebe8ce29c1a
NachoXx25 <ignacio.alfonso2002@gmail.com>
2025-04-30 20:23:25 -0400

    build: add microservices projects

---
+++ common/Messaging/RabbitMQConfiguration.cs
@@ -0,0 +1,10 @@
+namespace common.Messaging
+{
+    public class RabbitMQConfiguration
+    {
+        public string HostName { get; set; }
+        public string UserName { get; set; } 
+        public string Password { get; set; } 
+        public int Port { get; set; }
+    }
+}
