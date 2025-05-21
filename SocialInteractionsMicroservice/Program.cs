ad5211561326630c8272af4bb0510cecfc53d6d9
2kSebaNG <sebastian.nunez03@alumnos.ucn.cl>
2025-05-21 19:05:28 -0400

    feat: inject dependencies and configure services on Program.cs

--- SocialInteractionsMicroservice/Program.cs
+++ SocialInteractionsMicroservice/Program.cs
@@ -1,14 +1,31 @@
+using DotNetEnv;
+using Microsoft.EntityFrameworkCore;
 using SocialInteractionsMicroservice.Services;
+using SocialInteractionsMicroservice.src.Application.Services.Implements;
+using SocialInteractionsMicroservice.src.Application.Services.Interfaces;
+using SocialInteractionsMicroservice.src.Infrastructure.Data;
+using SocialInteractionsMicroservice.src.Infrastructure.Repositories.Implements;
+using SocialInteractionsMicroservice.src.Infrastructure.Repositories.Interfaces;
+
+Env.Load();
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddScoped<ISocialInteractionsRepository, SocialInteractionsRepository>();
+builder.Services.AddScoped<IVideoRepository, VideoRepository>();
+builder.Services.AddScoped<ISocialInteractionsService, SocialInteractionsService>();
 builder.Services.AddGrpc();
+builder.Services.AddDbContext<SocialInteractionsContext>(options => 
+    options.UseMongoDB(Env.GetString("MONGODB_CONNECTION"),Env.GetString("MONGODB_DATABASE_NAME")));
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
-app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+using (var scope = app.Services.CreateScope())
+{
+    await DataSeeder.Initialize(scope.ServiceProvider);
+}
+
+app.MapGrpcService<SocialInteractionsGrpcService>();
 
 app.Run();
