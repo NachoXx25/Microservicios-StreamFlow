dd84f7a2f82d2181006835126c77b1b1b3417fe7
2kSebaNG <sebastian.nunez03@alumnos.ucn.cl>
2025-05-21 13:06:46 -0400

    feat: inject dependencies and configure services in Program.cs

--- VideoMicroservice/Program.cs
+++ VideoMicroservice/Program.cs
@@ -1,14 +1,32 @@
+using DotNetEnv;
+using Grpc.Net.Client;
+using Microsoft.EntityFrameworkCore;
+using VideoMicroservice.Protos;
 using VideoMicroservice.Services;
+using VideoMicroservice.src.Application.Services.Implements;
+using VideoMicroservice.src.Application.Services.Interfaces;
+using VideoMicroservice.src.Infrastructure.Data;
+using VideoMicroservice.src.Infrastructure.Repositories.Implements;
+using VideoMicroservice.src.Infrastructure.Repositories.Interfaces;
+
+Env.Load();
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddScoped<IVideoService,VideoService>();
+builder.Services.AddScoped<IVideoRepository, VideoRepository>();
 builder.Services.AddGrpc();
+builder.Services.AddDbContext<VideoContext>(options => 
+    options.UseMongoDB(Env.GetString("MONGODB_CONNECTION"),Env.GetString("MONGODB_DATABASE_NAME")));
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
-app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+using (var scope = app.Services.CreateScope())
+{
+    await DataSeeder.Initialize(scope.ServiceProvider);
+}
 
+
+app.MapGrpcService<VideoMicroservice.Services.VideoGrpcService>();
 app.Run();
