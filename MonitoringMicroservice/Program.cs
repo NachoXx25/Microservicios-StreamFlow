02f1210ad5b50ae4232547aa466098b8ab8816d6
2kSebaNG <sebastian.nunez03@alumnos.ucn.cl>
2025-05-25 14:19:08 -0400

    feat: inject and configure services, stablish db connection and initialize data seeder

--- MonitoringMicroservice/Program.cs
+++ MonitoringMicroservice/Program.cs
@@ -1,14 +1,31 @@
+using DotNetEnv;
+using Microsoft.EntityFrameworkCore;
 using MonitoringMicroservice.Services;
+using MonitoringMicroservice.src.Application.Services.Implements;
+using MonitoringMicroservice.src.Application.Services.Interfaces;
+using MonitoringMicroservice.src.Infrastructure.Data;
+using MonitoringMicroservice.src.Infrastructure.Repositories.Implements;
+using MonitoringMicroservice.src.Infrastructure.Repositories.Interfaces;
+
+Env.Load();
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddScoped<IActionRepository, ActionRepository>();
+builder.Services.AddScoped<IErrorRepository, ErrorRepository>();
+builder.Services.AddScoped<IMonitoringService, MonitoringService>();
 builder.Services.AddGrpc();
+
+builder.Services.AddDbContext<MonitoringContext>(options => 
+    options.UseMongoDB(Env.GetString("MONGODB_CONNECTION"),Env.GetString("MONGODB_DATABASE_NAME")));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
-app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+using (var scope = app.Services.CreateScope())
+{
+    await DataSeeder.Initialize(scope.ServiceProvider);
+}
+
+app.MapGrpcService<MonitoringGrpcService>();
 
 app.Run();
