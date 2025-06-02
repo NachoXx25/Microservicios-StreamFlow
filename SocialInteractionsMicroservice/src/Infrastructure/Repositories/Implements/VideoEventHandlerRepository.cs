853df4c79e5017c1a598dad1a0f4fbf9e6b56da4
2kSebaNG <sebastian.nunez03@alumnos.ucn.cl>
2025-06-02 17:33:18 -0400

    feat: send the bson id to find videos on each method, also add the TriggerSeedersIfNeeded class to run the like and comments seeders only when there are enough videos

--- SocialInteractionsMicroservice/src/Infrastructure/Repositories/Implements/VideoEventHandlerRepository.cs
+++ SocialInteractionsMicroservice/src/Infrastructure/Repositories/Implements/VideoEventHandlerRepository.cs
@@ -2,7 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Bogus;
+using Microsoft.EntityFrameworkCore;
 using Serilog;
+using SocialInteractionsMicroservice.Services;
 using SocialInteractionsMicroservice.src.Domain.Models;
 using SocialInteractionsMicroservice.src.Infrastructure.Data;
 using SocialInteractionsMicroservice.src.Infrastructure.MessageBroker.Models;
@@ -14,9 +17,12 @@ namespace SocialInteractionsMicroservice.src.Infrastructure.Repositories.Impleme
     {
         private readonly SocialInteractionsContext _context;
 
-        public VideoEventHandlerRepository(SocialInteractionsContext context)
+        private readonly ISocialInteractionsEventService _socialInteractionsEventService;
+
+        public VideoEventHandlerRepository(SocialInteractionsContext context, ISocialInteractionsEventService socialInteractionsEventService)
         {
             _context = context;
+            _socialInteractionsEventService = socialInteractionsEventService;
         }
 
         public async Task HandleVideoCreatedEventAsync(VideoCreatedEvent videoCreatedEvent)
@@ -24,18 +30,17 @@ namespace SocialInteractionsMicroservice.src.Infrastructure.Repositories.Impleme
 
             try
             {
-
                 Log.Information("Video creado: {@VideoCreatedEvent}", videoCreatedEvent);
 
-                var existingVideo = await _context.Videos.FindAsync(videoCreatedEvent.Id);
+                var bsonId = new MongoDB.Bson.ObjectId(videoCreatedEvent.Id);
+
+                var existingVideo = await _context.Videos.FindAsync(bsonId);
 
                 if (existingVideo != null)
                 {
-
-                    throw new Exception("El video ya existe en la base de datos.");
+                    Log.Warning("El video con ID {VideoId} ya existe en la base de datos.", videoCreatedEvent.Id);
+                    return;
                 }
-
-                var bsonId = new MongoDB.Bson.ObjectId(videoCreatedEvent.Id);
 
                 await _context.Videos.AddAsync(new Video
                 {
@@ -47,6 +52,22 @@ namespace SocialInteractionsMicroservice.src.Infrastructure.Repositories.Impleme
                 });
                 await _context.SaveChangesAsync();
                 Log.Information("Video creado exitosamente: {@VideoCreatedEvent}", videoCreatedEvent);
+
+                var videoCount = await _context.Videos.CountAsync();
+                if (videoCount < 400 || videoCount > 600)
+                {
+                    Log.Information("No se cumplen las condiciones para ejecutar los seeders de interacciones sociales. Cantidad de videos: {VideoCount}", videoCount);
+                    return;
+                }
+                else
+                {
+                    if (await _context.Likes.AnyAsync() || await _context.Comments.AnyAsync())
+                    {
+                        Log.Information("Ya existen interacciones sociales, omitiendo seeders.");
+                        return;
+                    }
+                    await TriggerSeedersIfNeeded();
+                }
             }
             catch (Exception ex)
             {
@@ -61,12 +82,14 @@ namespace SocialInteractionsMicroservice.src.Infrastructure.Repositories.Impleme
             {
                 Log.Information("Video eliminado: {@VideoDeletedEvent}", videoDeletedEvent);
 
-                var existingVideo = await _context.Videos.FindAsync(videoDeletedEvent.Id) ?? throw new Exception("El video no existe en la base de datos.");
+                var bsonId = new MongoDB.Bson.ObjectId(videoDeletedEvent.Id);
+
+                var existingVideo = await _context.Videos.FindAsync(bsonId) ?? throw new KeyNotFoundException("El video no existe en la base de datos.");
 
                 existingVideo.IsDeleted = true;
 
                 await _context.SaveChangesAsync();
-                
+
                 Log.Information("Video eliminado con ID {VideoId}", videoDeletedEvent.Id);
             }
             catch (Exception ex)
@@ -82,14 +105,16 @@ namespace SocialInteractionsMicroservice.src.Infrastructure.Repositories.Impleme
             {
                 Log.Information("Video actualizado: {@VideoUpdatedEvent}", videoUpdatedEvent);
 
-                var existingVideo = await _context.Videos.FindAsync(videoUpdatedEvent.Id) ?? throw new Exception("El video no existe en la base de datos.");
+                var bsonId = new MongoDB.Bson.ObjectId(videoUpdatedEvent.Id);
+
+                var existingVideo = await _context.Videos.FindAsync(bsonId) ?? throw new KeyNotFoundException("El video no existe en la base de datos.");
 
                 existingVideo.Title = videoUpdatedEvent.Title;
                 existingVideo.Description = videoUpdatedEvent.Description;
                 existingVideo.Genre = videoUpdatedEvent.Genre;
 
                 await _context.SaveChangesAsync();
-                Log.Information("Usuario actualizado con ID {VideoId}", videoUpdatedEvent.Id);
+                Log.Information("Video actualizado con ID {VideoId}", videoUpdatedEvent.Id);
             }
             catch (Exception)
             {
@@ -97,5 +122,58 @@ namespace SocialInteractionsMicroservice.src.Infrastructure.Repositories.Impleme
                 throw;
             }
         }
+        
+        private async Task TriggerSeedersIfNeeded()
+        {
+            try
+            {
+                var videoIds = await _context.Videos
+                                        .Select(v => v.Id.ToString())
+                                        .ToListAsync();
+
+                if (!await _context.Likes.AnyAsync())
+                {
+                    Log.Information("Ejecutando seeders de likes...");
+                    // Generar Likes
+                    var fakerLikes = new Faker<Like>()
+                        .RuleFor(l => l.VideoId, f => f.PickRandom(videoIds));
+
+                    await _context.Likes.AddRangeAsync(fakerLikes.Generate(75));
+                    await _context.SaveChangesAsync();
+                    Log.Information("Seeders de Likes ejecutados exitosamente.");
+
+                    var likes = await _context.Likes.AsNoTracking().ToListAsync();
+                    foreach (var like in likes)
+                    {
+                        await _socialInteractionsEventService.PublishLikeEvent(like.VideoId.ToString(), like.Id.ToString());
+                    }
+                }
+                else
+                {
+                    Log.Information("Ya existen likes, omitiendo seeder de likes");
+                }
+
+                if (!await _context.Comments.AnyAsync())
+                {
+                    Log.Information("Ejecutando seeders de comentarios...");
+                    // Generar Comentarios
+                    var fakerComments = new Faker<Comment>()
+                        .RuleFor(c => c.VideoId, f => f.PickRandom(videoIds))
+                        .RuleFor(c => c.Content, f => f.Lorem.Sentence(10));
+
+                    await _context.Comments.AddRangeAsync(fakerComments.Generate(35));
+                    await _context.SaveChangesAsync();
+                    Log.Information("Seeders de Comentarios ejecutados exitosamente.");
+                }
+                else
+                {
+                    Log.Information("Ya existen comentarios, omitiendo seeder de comentarios");
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "Error ejecutando seeders de interacciones");
+            }
+        }   
     }
 }
