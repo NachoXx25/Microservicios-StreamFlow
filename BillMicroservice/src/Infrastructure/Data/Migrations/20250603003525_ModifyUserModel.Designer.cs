7c8e354418d74f7a71918444c793619211b61f41
2kSebaNG <sebastian.nunez03@alumnos.ucn.cl>
2025-06-02 20:36:07 -0400

    feat: update migrations with the modified User's model

--- BillMicroservice/src/Infrastructure/Data/Migrations/20250603003525_ModifyUserModel.Designer.cs
+++ BillMicroservice/src/Infrastructure/Data/Migrations/20250603003525_ModifyUserModel.Designer.cs
@@ -4,6 +4,7 @@ using BillMicroservice.src.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BillMicroservice.src.Infrastructure.Data.Migrations
 {
     [DbContext(typeof(BillContext))]
-    partial class BillContextModelSnapshot : ModelSnapshot
+    [Migration("20250603003525_ModifyUserModel")]
+    partial class ModifyUserModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
