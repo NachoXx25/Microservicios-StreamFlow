18e289b231b1cc9c59fd54d979afca2e70480fdf
2kSebaNG <sebastian.nunez03@alumnos.ucn.cl>
2025-06-02 20:34:22 -0400

    refactor: update the User model to include name, lastname and status

--- BillMicroservice/src/Domain/Models/User/User.cs
+++ BillMicroservice/src/Domain/Models/User/User.cs
@@ -8,7 +8,9 @@ namespace BillMicroservice.src.Domain.Models.User
 {
     public class User : IdentityUser<int>
     {
-
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required bool Status { get; set; }
         public required int RoleId { get; set; }
         public Role Role { get; set; } = null!;
     }
