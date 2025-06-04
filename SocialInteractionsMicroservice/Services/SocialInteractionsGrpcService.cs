16f540c5c2cb86e260eb417d7c926093510661c5
2kSebaNG <sebastian.nunez03@alumnos.ucn.cl>
2025-06-03 23:21:49 -0400

    feat: update the SocialInteractions proto to include UserData for the MonitoringService. Also update the gRPC service to send the error and actions events to the MonitoringMicroservice

--- SocialInteractionsMicroservice/Services/SocialInteractionsGrpcService.cs
+++ SocialInteractionsMicroservice/Services/SocialInteractionsGrpcService.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Grpc.Core;
 using SocialInteractionsMicroservice.src.Application.Services.Interfaces;
 using SocialInteractionsMicroservice.src.Domain.Models;
+using SocialInteractionsMicroservice.src.Infrastructure.MessageBroker.Models;
 
 namespace SocialInteractionsMicroservice.Services
 {
@@ -12,16 +13,33 @@ namespace SocialInteractionsMicroservice.Services
     {
         private readonly ISocialInteractionsService _socialInteractionsService;
 
-        public SocialInteractionsGrpcService(ISocialInteractionsService socialInteractionsService)
+        private readonly IMonitoringEventService _monitoringEventService;
+
+        public SocialInteractionsGrpcService(ISocialInteractionsService socialInteractionsService, IMonitoringEventService monitoringEventService)
         {
             _socialInteractionsService = socialInteractionsService;
+            _monitoringEventService = monitoringEventService;
         }
 
         public override async Task<Protos.GetVideoLikesAndCommentsResponse> GetVideoLikesAndComments(Protos.GetVideoLikesAndCommentsRequest request, ServerCallContext context)
         {
             try
             {
+                await _monitoringEventService.PublishActionEventAsync(new ActionEvent
+                {
+                    ActionMessage = "Obtener likes y comentarios de un video",
+                    Service = "SocialInteractionsMicroservice",
+                    UserId = request.UserData.Id,
+                    UserEmail = request.UserData.Email,
+                    UrlMethod = $"GET/interacciones/{request.VideoId}"
+                });
+
                 var response = await _socialInteractionsService.GetVideoInteractions(request.VideoId);
+
+                if (response == null)
+                {
+                    throw new KeyNotFoundException("No se encontraron interacciones para el video especificado.");
+                }
 
                 var likes = response.Likes.Select(l => new Protos.Like
                 {
@@ -41,17 +59,43 @@ namespace SocialInteractionsMicroservice.Services
                     Comments = { comments }
                 };
             }
+            catch (KeyNotFoundException knfEx)
+            {
+                await _monitoringEventService.PublishErrorEventAsync(new ErrorEvent
+                {
+                    ErrorMessage = knfEx.Message,
+                    Service = "SocialInteractionsMicroservice",
+                    UserId = request.UserData.Id,
+                    UserEmail = request.UserData.Email,
+                });
+                throw new RpcException(new Status(StatusCode.NotFound, knfEx.Message));
+            }
             catch (Exception ex)
             {
+                await _monitoringEventService.PublishErrorEventAsync(new ErrorEvent
+                {
+                    ErrorMessage = $"Error al obtener likes y comentarios de un video:{ex.Message}",
+                    Service = "SocialInteractionsMicroservice",
+                    UserId = request.UserData.Id,
+                    UserEmail = request.UserData.Email,
+                });
                 throw new RpcException(new Status(StatusCode.Internal, ex.Message));
             }
         }
 
         public override async Task<Protos.GiveLikeResponse> GiveLike(Protos.GiveLikeRequest request, ServerCallContext context)
         {
-
             try
             {
+                await _monitoringEventService.PublishActionEventAsync(new ActionEvent
+                {
+                    ActionMessage = "Dar like",
+                    Service = "SocialInteractionsMicroservice",
+                    UserId = request.UserData.Id,
+                    UserEmail = request.UserData.Email,
+                    UrlMethod = $"POST/interacciones/{request.VideoId}/likes"
+                });
+
                 var response = await _socialInteractionsService.GiveLike(request.VideoId);
                 return new Protos.GiveLikeResponse
                 {
@@ -61,15 +105,30 @@ namespace SocialInteractionsMicroservice.Services
             }
             catch (Exception ex)
             {
+                await _monitoringEventService.PublishErrorEventAsync(new ErrorEvent
+                {
+                    ErrorMessage = $"Error al dar like al video: {ex.Message}",
+                    Service = "SocialInteractionsMicroservice",
+                    UserId = request.UserData.Id,
+                    UserEmail = request.UserData.Email,
+                });
                 throw new RpcException(new Status(StatusCode.Internal, ex.Message));
             }
         }
 
         public override async Task<Protos.MakeCommentResponse> MakeComment(Protos.MakeCommentRequest request, ServerCallContext context)
         {
-
             try
             {
+                await _monitoringEventService.PublishActionEventAsync(new ActionEvent
+                {
+                    ActionMessage = "Dejar comentario",
+                    Service = "SocialInteractionsMicroservice",
+                    UserId = request.UserData.Id,
+                    UserEmail = request.UserData.Email,
+                    UrlMethod = $"POST/interacciones/{request.VideoId}/comentarios"
+                });
+
                 var response = await _socialInteractionsService.MakeComment(request.VideoId, request.Comment);
                 return new Protos.MakeCommentResponse
                 {
@@ -79,6 +138,13 @@ namespace SocialInteractionsMicroservice.Services
             }
             catch (Exception ex)
             {
+                await _monitoringEventService.PublishErrorEventAsync(new ErrorEvent
+                {
+                    ErrorMessage = $"Error al dejar comentario en el video: {ex.Message}",
+                    Service = "SocialInteractionsMicroservice",
+                    UserId = request.UserData.Id,
+                    UserEmail = request.UserData.Email,
+                });
                 throw new RpcException(new Status(StatusCode.Internal, ex.Message));
             }
         }
